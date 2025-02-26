1f9e34197d3b15a812e0c61c66c0c7f4833d664b
BingrongSu <robertsu1121@outlook.com>
2025-02-26 10:16:38 +0800

    update

--- src/main/java/net/robert/mcduro/game/ModDamageSources.java
+++
@@ -1,7 +0,0 @@
-package net.robert.mcduro.game;
-
-import net.minecraft.entity.damage.DamageSource;
-
-public class ModDamageSources {
-    public static final DamageSource FH_SKILL_5 = new DamageSource("custom_damage");
-}
--- src/main/java/net/robert/mcduro/mixin/LivingEntityMixin.java
+++ src/main/java/net/robert/mcduro/mixin/LivingEntityMixin.java
@@ -2,15 +2,40 @@ package net.robert.mcduro.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.robert.mcduro.entity.custom.SkillFH5Ball;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Inject(at = @At("HEAD"), method = "damage", cancellable = true)
-    public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    @Shadow public abstract boolean damage(DamageSource source, float amount);
 
+    @Shadow public abstract void damageArmor(DamageSource source, float amount);
+
+    @Unique
+    private float damage = 0f;
+
+    @ModifyVariable(method = "damage", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private float modifyAmount(float amount) {
+        if (amount == -11.21f) {
+            float ans = amount + damage;
+            damage = 0;
+            return ans;
+        } else {
+            return amount;
+        }
+    }
+
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source.getSource() instanceof SkillFH5Ball) {
+            cir.cancel();
+        }
+        System.out.println(source.getType());
     }
 }
